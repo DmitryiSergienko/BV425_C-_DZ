84752778f113558cc063a0a573ab56aa98434c25
Dimka <godslim1992@gmail.com>
2025-04-13 03:31:16 +0300

    Practice Snake Matrix

--- Program.cs
+++ Program.cs
@@ -1,30 +1,80 @@
-﻿using BV425_C__DZ.Bridge.Classes;
-using BV425_C__DZ.Flyweight.Classes;
+﻿// Вариант 3: Заполнение матрицы змейкой по диагонали
+int Y = 4;
+int X = 5;
+int[,] matrix = new int[Y, X];
 
-Console.WriteLine("Паттерн \"Мост\" (Bridge)\n" +
-    "Пример: Рендеринг фигур с различными цветами");
+int minSize = Y;
+int maxSize = X;
+if (X < Y)
+{
+    minSize = X;
+    maxSize = Y;
+}
+int firstLastDiag = minSize - 1;
 
-var svgRenderer = new SvgRenderer();
-var canvasRenderer = new CanvasRenderer();
+int iter = X * Y;
+int iter2 = iter;
+int shiftRight = 0;
+int shiftLeft = 0;
 
-var circle = new Circle(svgRenderer);
-circle.Draw();
+for (int i = 0; i < Y + X - 1; i++) // Количество диагоналей
+{
+    if (i >= X)
+    {
+        shiftRight++;
+    }
+    if (i >= Y)
+    {
+        shiftLeft++;
+    }
+    for (int j = 0; j < minSize - firstLastDiag; j++) // Длина диагонали
+    {
+        if (i % 2 == 1)
+        {
+            matrix[j + shiftRight, i - j - shiftRight] = iter;
+            //Console.Write(matrix[j + shiftRight, i - j - shiftRight]);
+        }
+        else
+        {
+            matrix[i - j - shiftLeft, j + shiftLeft] = iter;
+            //Console.Write(matrix[i - j - shiftLeft, j + shiftLeft]);
+        }
+        iter--;
+        iter2--;
 
-var rectangle = new Rectangle(canvasRenderer);
-rectangle.Draw();
+        for (int y = 0; y < Y; y++)
+        {
+            for (int x = 0; x < X; x++)
+            {
+                if (matrix[y, x] == 0)
+                {
+                    break;
+                }
+                Console.Write(matrix[y, x] - iter2 + "\t");
+            }
+            Console.WriteLine();
+        }
+        Thread.Sleep(500);
+        Console.Clear();
+    }
+    //Console.WriteLine();
 
-///////////////////////////////////////////////////////////////////
+    if (i < minSize - 1) // Нарщиваем диагональ
+    {
+        firstLastDiag--;
+    }
+    else if (i > maxSize - 2) // Уменьшаем диагональ
+    {
+        firstLastDiag++;
+    }
+}
 
-Console.WriteLine("\n===========================================\n" +
-    "Паттерн \"Легковес\" (Flyweight)\n" +
-    "Пример: Управление символами в текстовом редакторе");
-
-var factory = new CharacterFactory();
-var editor = new TextEditor(factory);
-
-editor.AddCharacter('A', 0);
-editor.AddCharacter('B', 1);
-editor.AddCharacter('A', 2); // 'A' уже существует, будет использован существующий легковес
-editor.AddCharacter('C', 3);
-
-editor.Display();
+//Console.WriteLine();
+for (int i = 0; i < Y; i++)
+{
+    for (int j = 0; j < X; j++)
+    {
+        Console.Write(matrix[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
